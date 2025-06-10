118d0019ca256a5e8120891d4d515b4753f37dca
Ben Russell <br@x-plugins.com>
2025-06-11 02:15:20 +1000

    better plugin info query fns

--- xp_sdk_shim/xp_core.cpp
+++ xp_sdk_shim/xp_core.cpp
@@ -12,6 +12,14 @@
 
 
 
+#include "../src/Plugin.h"
+
+#include "../src/XPHost.h"
+
+
+
+
+
 [[maybe_unused]] void XPLMEnableFeature( const char* feature_name ){
 	if( strcmp(feature_name, "XPLM_USE_NATIVE_PATHS") != 0 ){
 		// mute the native paths request as thats all we support
@@ -47,7 +55,12 @@
 
 
 [[maybe_unused]] int XPLMGetMyID( void ){
-	return 42; //FIXME: return a useful ID
+	std::cout << "xwb/ XPLMGetMyID: [" <<
+			global_target_plugin->m_plugin_id <<
+			"] " <<
+			global_target_plugin->m_pluginSig <<
+			".\n";
+	return global_target_plugin->m_plugin_id;
 }
 
 [[maybe_unused]] void XPLMGetPluginInfo( 
@@ -58,31 +71,39 @@
     char* outDescription
 ){
 
-	//FIXME: lookup the plugin info struct with pluginId
+	int targetId = pluginId - 1;
+	if ( targetId >= XPHost::m_vecPlugins.size() ) {
+		std::cerr << "XPLMGetPluginInfo: targetId is out of bounds.\n";
+		return;
+	}
+	auto plugin = XPHost::m_vecPlugins[targetId];
 
 	if( outName ){
-		//set
+		snprintf(outName,256,
+			"%s",
+			plugin->m_pluginName.c_str()
+		);
 	}
 
 	if( outFilePath ){
-		//set
-		//snprintf(outFilePath,256,"/home/br/Dev/wasm/wasm_host_xpl/build/xpl/libwasm_xpl_plugin.so");
-		
-		printf("xwb/ dyn resolved xpl filename! - yay!\n");
 		snprintf(outFilePath,256,
 			"%s",
-			global_target_plugin->m_pluginFilename.c_str()
+			plugin->m_pluginFilename.c_str()
 		);
-
-
 	}
 
 	if( outSignature ){
-		//set
+		snprintf(outSignature,256,
+			"%s",
+			plugin->m_pluginSig.c_str()
+		);
 	}
 
 	if( outDescription ){
-		//set
+		snprintf(outDescription,256,
+			"%s",
+			plugin->m_pluginDesc.c_str()
+		);
 	}
 
 }
